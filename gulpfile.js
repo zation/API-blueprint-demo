32ac20be1f58691a2c406441b23df8502a0c4647
Liu Yang <zation1@gmail.com>
2014-12-14 14:43:28 +0800

    build API HTML and add live reload

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp');
+var aglio = require('gulp-aglio');
+var connect = require('gulp-connect');
+var watch = require('gulp-watch');
+
+gulp.task('document', function() {
+  return gulp.src(['documents/API.md'])
+    .pipe(watch('documents/API.md'))
+    .pipe(aglio({template: 'default', filename: './.tmp/index.html'}))
+    .pipe(gulp.dest('./.tmp/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('server', function() {
+  return connect.server({
+    root: ['./.tmp'],
+    port: 3456,
+    livereload: {
+      port: 35728
+    }
+  });
+});
+
+var gopen = require('gulp-open');
+
+gulp.task('open', function() {
+  return gulp.src('./gulpfile.js')
+    .pipe(gopen('', {
+      url: 'http://localhost:3456'
+    }));
+});
+
+gulp.task('default', ['server', 'document', 'open']);
